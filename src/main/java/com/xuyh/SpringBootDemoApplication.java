cbc188fc1b75c4859baa09bf7c7e6b22e3b03e08
xuyh <603559825@qq.com>
2018-12-12 21:02:26 +0800

    增加tomcat启动方式

--- src/main/java/com/xuyh/SpringBootDemoApplication.java
+++ src/main/java/com/xuyh/SpringBootDemoApplication.java
@@ -2,10 +2,16 @@ package com.xuyh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(SpringBootDemoApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
