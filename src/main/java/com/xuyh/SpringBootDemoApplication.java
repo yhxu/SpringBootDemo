fe6e972c45f3ed7edc85c37ac80b2f3f4a84cb14
yuyh <603559825@qq.com>
2018-12-27 11:27:47 +0800

    启用定时任务

--- src/main/java/com/xuyh/SpringBootDemoApplication.java
+++ src/main/java/com/xuyh/SpringBootDemoApplication.java
@@ -3,11 +3,15 @@ package com.xuyh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
+@EnableScheduling
+@EnableConfigurationProperties
 public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
 	@Override
