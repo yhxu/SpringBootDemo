f3c1010bcc2017e9dc3e5814ec7e2a6362b33806
yuyh <603559825@qq.com>
2018-12-28 16:21:27 +0800

    获取项目各类定时任务数量

--- src/main/java/com/xuyh/timers/MySchedulingConfigurer.java
+++ src/main/java/com/xuyh/timers/MySchedulingConfigurer.java
@@ -1,5 +1,7 @@
 package com.xuyh.timers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
@@ -15,12 +17,18 @@ import java.util.concurrent.Executors;
  */
 @Configuration
 public class MySchedulingConfigurer implements SchedulingConfigurer {
+    private static final Logger mLogger = LoggerFactory.getLogger(MySchedulingConfigurer.class);
+
     // 获取自定义参数
     @Value("${timer.pool.number}")
     private int numbers;
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        mLogger.info("number of triggerTasks is    :" + scheduledTaskRegistrar.getTriggerTaskList().size());
+        mLogger.info("number of cronTasks is       :" + scheduledTaskRegistrar.getCronTaskList().size());
+        mLogger.info("number of fixedRateTasks is  :" + scheduledTaskRegistrar.getFixedRateTaskList().size());
+        mLogger.info("number of fixedDelayTasks is :" + scheduledTaskRegistrar.getFixedDelayTaskList().size());
         scheduledTaskRegistrar.setScheduler(Executors.newScheduledThreadPool(numbers));
     }
 }
