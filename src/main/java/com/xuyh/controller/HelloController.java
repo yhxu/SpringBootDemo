4898f87af3ccbda53bc7acec9140c5fea0cfa407
yuyh <603559825@qq.com>
2019-01-15 10:49:47 +0800

    增加locale可用方法说明

--- src/main/java/com/xuyh/controller/HelloController.java
+++ src/main/java/com/xuyh/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.xuyh.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -14,6 +15,7 @@ import java.util.Locale;
  * @Date: 2018/12/12
  * @Version:
  */
+@Slf4j
 @RestController
 public class HelloController {
     @Autowired
@@ -27,6 +29,23 @@ public class HelloController {
     @GetMapping(value = "/i18n")
     public String i18nController(){
         Locale locale = LocaleContextHolder.getLocale();
+        log.info("is simplified Chinese:" +locale.equals(Locale.SIMPLIFIED_CHINESE)); // is simplified Chinese:true
+        log.info("the localed language is :" +locale.getLanguage()); // the localed language is :zh
+        log.info("the display language is :" +locale.getDisplayLanguage()); // the display language is :中文
+        log.info("the localed country is :" +locale.getCountry()); // the localed country is :CN
+        log.info("the display country is :" +locale.getDisplayCountry()); // the display country is :中国
+        log.info("the localed script is :" +locale.getScript()); // the localed script is :
+        log.info("the display script is :" +locale.getDisplayScript()); // the display script is :
+        log.info("the display name is :" +locale.getDisplayName()); // the display name is :中文 (中国)
+        log.info("the display variant is :" +locale.getDisplayVariant()); // the display variant is :
+        log.info("the localed variant is :" +locale.getVariant()); // the localed variant is :
+        log.info("toString :" +locale.toString()); // toString :zh_CN
+        log.info("the ISO3 country is :" +locale.getISO3Country()); // the ISO3 country is :CHN
+        log.info("the ISO3 language is :" +locale.getISO3Language()); // the ISO3 language is :zho
+        log.info("toLanguageTag :" +locale.toLanguageTag()); // toLanguageTag :zh-CN
+        log.info("ExtensionKeys :" +locale.getExtensionKeys().toString()); // ExtensionKeys :[]
+        log.info("UnicodeLocaleKeys :" +locale.getUnicodeLocaleKeys().toString()); // UnicodeLocaleKeys :[]
+        log.info("UnicodeLocaleAttributes :" +locale.getUnicodeLocaleAttributes().toString()); // UnicodeLocaleAttributes :[]
         return messageSource.getMessage("international.message", null, "默认值", locale);
     }
 }
