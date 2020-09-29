8972ea25263cd7e1868d5708bfe98e89eb53b5d1
xuyh <603559825@qq.com>
2020-09-29 13:54:07 +0800

    dao可以不加注解

--- src/main/java/com/xuyh/dao/impl/UserDaoImpl.java
+++ src/main/java/com/xuyh/dao/impl/UserDaoImpl.java
@@ -2,11 +2,9 @@ package com.xuyh.dao.impl;
 
 import com.xuyh.dao.UserDao;
 import com.xuyh.model.UserModel;
-import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 
-@Service
 public class UserDaoImpl extends BaseDaoImpl<UserModel> implements UserDao {
 
     public UserDaoImpl(EntityManager entityManager) {
